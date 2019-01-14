ce9dfe935975bdc4b2fa9fb8c3aa5ef8e3e868fa
romene <rogerio.eire@gmail.com>
2019-01-14 11:24:14 +0700

    Intro to Redux 2

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { Button, Form, FormGroup, Label, Col, Input, FormFeedback} from "reactstrap";
-import { Link } from "react-router-dom";
 
 class Contact extends Component {
     constructor(props) {
